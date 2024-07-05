d92fd5ec5c560339c60804534325779c06cd049a
shankar011 <108754490+shankar011@users.noreply.github.com>
2024-07-06 01:10:18 +0530

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-// module.exports = {
-//     // Enable static HTML export
-//     target: 'serverless',
-//     distDir: 'out', // The directory where the exported files will be stored
-//     // Other configuration options
-//   };
+const nextConfig = {
+    reactStrictMode: true,
+  };
   
+  export default nextConfig;
+  
+
